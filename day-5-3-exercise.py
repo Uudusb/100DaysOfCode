3844a2b6342903c2ac6634d73098d0534ab5de93
Uudus Bayarsaikhan <49882287+Uudusb@users.noreply.github.com>
2021-02-19 21:04:50 -0600

    Create day-5-3-exercise.py

---
+++ day-5-3-exercise.py
@@ -0,0 +1,11 @@
+#Write your code below this row 👇
+sumOfEven = 0
+for even in range(2, 101, 2):
+  sumOfEven += even
+
+print(sumOfEven)#Write your code below this row 👇
+sumOfEven = 0
+for even in range(2, 101, 2):
+  sumOfEven += even
+
+print(sumOfEven)
